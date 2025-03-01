a199d71f34eccd1c710d2142c2a42134044c14f9
romeq41 <jakubromanowski41@gmail.com>
2025-03-01 19:39:01 +0100

    Final Commit

--- Shoppy-backend/config/env.php
+++
@@ -1,8 +0,0 @@
-<?php
-    return [
-        'JWT_SECRET_KEY' => '123456',
-        'Host' => 'smtp.poczta.onet.pl',
-        'Username' => 'shoppy.customer.service@op.pl',
-        'Password' => '452319klucZ',
-        'Port' => 465
-    ];
